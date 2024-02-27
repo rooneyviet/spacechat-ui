d512d9db03d5596d40451ebeac01205b90cafbf7
Nguyen Viet Quan <nguyenvietquan0304@gmail.com>
2024-02-27 23:49:34 +0900

    add mantine

---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SideBar = () => {
+  return (
+    <ul>
+      <li>Home</li>
+      <li>Home1</li>
+      <li>Home1</li>
+    </ul>
+  );
+};
+
+export default SideBar;
